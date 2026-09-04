0715a86a1406ad900bc6678ca5989df656afb35f
Ericsosohugo1996 <ericbrunebarbe@gmail.com>
2026-09-04 23:07:16 +0200

    Fix formatting issues in app.js

--- app.js
+++ app.js
@@ -2161,7 +2161,8 @@ const CITY_PHOTOS = {
   bordeaux: "https://commons.wikimedia.org/wiki/Special:FilePath/138_-_Place_de_la_Bourse_et_le_miroir_d'eau_-_Bordeaux.jpg",
   toulouse: "https://commons.wikimedia.org/wiki/Special:FilePath/Toulouse_Capitole_Night_Wikimedia_Commons.jpg",
     marseille: "https://commons.wikimedia.org/wiki/Special:FilePath/Notre-Dame_de_la_Garde_aerial_view_2020_(cropped).jpeg",
-  sens: "https://commons.wikimedia.org/wiki/Special:FilePath/Cath%C3%A9drale_Saint-%C3%89tienne,_Sens-6998.jpg",
+    sens: "https://commons.wikimedia.org/wiki/Special:FilePath/Cath%C3%A9drale_Saint-%C3%89tienne,_Sens-6998.jpg",
+  brest: "https://commons.wikimedia.org/wiki/Special:FilePath/Chateau_de_Brest_et_Tour_Tanguy.jpg",
 };
 function renderLocateBar(){
   document.getElementById("locate-label").textContent = state.userPos ? "Position détectée" : "Ville sélectionnée";
