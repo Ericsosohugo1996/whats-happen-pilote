fbe1436af4920e16d6d5d05c2e8495f3a9075298
Ericsosohugo1996 <ericbrunebarbe@gmail.com>
2026-09-10 09:02:28 +0200

    Update event categories to include 'À voir'
    Added 'À voir' to the list of event categories.

--- app.js
+++ app.js
@@ -973,7 +973,7 @@ const SEED_EVENTS = [
   },
 ]; 
 
-const CATEGORIES = ["Musique", "Marché", "Brocante", "Festival", "Sport", "Soirée", "Expo"];
+const CATEGORIES = ["Musique", "Marché", "Brocante", "Festival", "Sport", "Soirée", "Expo", "À voir"];
 
 // Scène illustrée par défaut selon la catégorie (utilisée pour les événements publiés par les
 // utilisateurs, qui n'ont pas de scène assignée manuellement).
