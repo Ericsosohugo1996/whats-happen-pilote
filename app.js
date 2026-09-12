90d445f2a90496b1269498ceab62042376935338
Ericsosohugo1996 <ericbrunebarbe@gmail.com>
2026-09-12 17:04:00 +0200

    Add new Bordeaux entry to city data

--- app.js
+++ app.js
@@ -2266,6 +2266,7 @@ const OPENAGENDA_SOURCES = [
    { agendaId: 2404802,  cityName: "Brest",            cityKey: "brest", search: null },
   { agendaId: 83392987, cityName: "Bordeaux",         cityKey: "bordeaux", search: null, size: 300 },
     { agendaId: 94573624, cityName: "Bordeaux", cityKey: "bordeaux", search: null },
+    { agendaId: 19327333, cityName: "Bordeaux", cityKey: "bordeaux", search: null },
     { agendaId: 42448083, cityName: "Toulouse",         cityKey: "toulouse", search: null, size: 300 },
     { agendaId: 96398684, cityName: "Toulouse", cityKey: "toulouse", search: null },
          { agendaId: 21769447, cityName: "Marseille",        cityKey: "marseille", search: null, size: 100 },
