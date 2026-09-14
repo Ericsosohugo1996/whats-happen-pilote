b4a21c075926d77e6f6d9518e4db2101bf6576af
Ericsosohugo1996 <ericbrunebarbe@gmail.com>
2026-09-14 11:51:49 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -34,7 +34,7 @@ self.addEventListener("fetch", (event) => {
   if (!event.request.url.startsWith(self.location.origin)) return;
 
   event.respondWith(
-    fetch(event.request)
+        fetch(event.request, { cache: "no-cache" })
       .then((response) => {
         const responseClone = response.clone();
         caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseClone));
