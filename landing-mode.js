59044525fd226aa654fb92c5afeccec297b64b3c
Ericsosohugo1996 <ericbrunebarbe@gmail.com>
2026-09-14 13:49:13 +0200

    Update landing-mode.js

--- landing-mode.js
+++ landing-mode.js
@@ -115,9 +115,7 @@ function __landingModeOpen() {
     '<div style="color:rgba(255,255,255,0.65); font-size:12px; line-height:1.4; margin-bottom:16px;">' +
     contextLine +
     "</div>" +
-    '<div style="color:rgba(255,255,255,0.65); font-size:12.5px; line-height:1.4; margin-top:-10px; margin-bottom:16px;">' +
-    contextLine +
-    "</div>" +
+    
     '<div style="background:#fff; border-radius:18px; padding:0 16px;">' +
     cardsHTML +
     "</div>" +
