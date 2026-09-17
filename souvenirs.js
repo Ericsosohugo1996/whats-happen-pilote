e2f654e14d6c4fa84983622ca520d2ec7c81ad77
Ericsosohugo1996 <ericbrunebarbe@gmail.com>
2026-09-17 23:39:11 +0200

    Update souvenirs.js

--- souvenirs.js
+++ souvenirs.js
@@ -22,9 +22,12 @@
       const d = haversineKm(lat, lng, c.lat, c.lng);
       if (d < closestDist) { closestDist = d; closest = key; }
     });
-    return closest;
+      return closest;
   }
 
+    async function saveSouvenir({ file, text, placeName, placeId, lat, lng }) {
+
+    const user = auth.currentUser;
   async function saveSouvenir({ file, text, placeName, placeId, lat, lng }) {
     const user = auth.currentUser;
     if (!user) {
